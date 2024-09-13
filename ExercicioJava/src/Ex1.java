84a4788485f7a5ae029e80e3685e8f40c99129f8
airotivmaria <mariavitoria6019@gmail.com>
2024-09-13 11:03:05 -0300

    Troca de nome

--- ExercicioJava/src/Ex1.java
+++ ExercicioJava/src/Ex1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Ex1 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um número inteiro: ");
